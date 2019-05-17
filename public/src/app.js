ad9bc3925de30be0e9b16831c0b9ede357b7cf88
janarf <janarf@hotmail.com>
2019-05-17 13:41:52 -0300

    testes com api. api here funcionando, retorna tempo para 3 tipos de transporte

--- public/src/app.js
+++ public/src/app.js
@@ -1,141 +1,140 @@
+
+function getRouteTime(waypointString0, waypointString1) {
+    fetch(`https://route.api.here.com/routing/7.2/calculateroute.json
+?app_id=lT3yqAJmOo0tSCEDAY24
+&app_code=fd9_LnoGgmj6hkcLl2RhNQ
+&waypoint0=${waypointString0}
+&waypoint1=${waypointString1}
+&routeattributes=wp,sm,sh,sc
+&mode=fastest;${$('#transport').val()}`).then(res => res.json())
+        .then(res => {
+            if ($('#transport').val() == 'car') {
+                console.log(res.response.route[0].summary.trafficTime)
+            } else {
+                console.log(res.response.route[0].summary.baseTime)
+
+            }
+        })
+}
+
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(getPosition);
     } else {
         console.log("Geolocation is not supported by this browser.")
     }
 }
-
-function getSearchParams() {
-    $("#address-search").on('click', (e) => {
-        e.preventDefault();
-        getLocation();
-    })
-}
-function setDestiny(geocoder, geocodingParams) {
-    let destiny;
-    geocoder.geocode(geocodingParams, (result) => {
-        destiny = `geo!${result.Response.View[0].Result[0].Location.DisplayPosition.Latitude},${result.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`
-    }, function (e) {
-        alert(e);
-    })
-    if (destiny) {
-        return destiny;
-    }
-
-
-}
-function showPosition(positionUser) {
-    var geocodingParams = {
-        searchText: $("#address-search-bar").val()
-    };
-
+function getPosition(userPosition) {
     var platform = new H.service.Platform({
         'app_id': 'lT3yqAJmOo0tSCEDAY24',
         'app_code': 'fd9_LnoGgmj6hkcLl2RhNQ'
     });
-    // Retrieve the target element for the map:
-    var targetElement = document.getElementById('mapContainer');
-    var geocoder = platform.getGeocodingService();
-
-    // Call the geocode method with the geocoding parameters,
-    // the callback and an error callback function (called if a
-    // communication error occurs):
-
-
-    // Get the default map types from the platform object:
     var defaultLayers = platform.createDefaultLayers();
-
-    // Instantiate the map:
-    var map = new H.Map(
-        document.getElementById('mapContainer'),
+    let map = new H.Map(
+        $('#mapContainer')[0],
         defaultLayers.normal.map, {
-            zoom: 10,
-            center: { lat: positionUser.coords.latitude, lng: positionUser.coords.longitude }
+            zoom: 15,
+            center: { lat: userPosition.coords.latitude, lng: userPosition.coords.longitude }
         });
+    var marker = new H.map.Marker({
+        lat: userPosition.coords.latitude,
+        lng: userPosition.coords.longitude
+    });
+    map.addObjects([marker])
 
-    let userpoint = `geo!${positionUser.coords.latitude},${positionUser.coords.longitude}`
-    let destiny = setDestiny(geocoder, geocodingParams)
-    console.log(userpoint, geocoder, geocodingParams)
-    // Create the parameters for the routing request:
-    var routingParameters = {
-        // The routing mode:
-        'mode': 'fastest;car;traffic:enabled',
-        // The start point of the route:
-        'waypoint0': userpoint,
-        // The end point of the route:
-        'waypoint1': `geo!-23.5676551,-46.6980049`,
-        // To retrieve the shape of the route we choose the route
-        // representation mode 'display'
-        'representation': 'display',
+    $("#address-search").on('click', (e) => {
+        e.preventDefault();
+        var geocodingParams = {
+            searchText: $("#address-search-bar").val()
+        };
+        var geocoder = platform.getGeocodingService();
+        let userpoint = `geo!${userPosition.coords.latitude},${userPosition.coords.longitude}`
+        console.log($("#address-search-bar").val().split(' ').join('+'))
 
+        fetch(`https://geocoder.api.here.com/6.2/geocode.json?app_id=lT3yqAJmOo0tSCEDAY24&app_code=fd9_LnoGgmj6hkcLl2RhNQ&searchtext=${$("#address-search-bar").val().split(' ').join('+')}`)
+            .then(res => res.json()).then(result => {
+                let destiny = `geo!${result.Response.View[0].Result[0].Location.DisplayPosition.Latitude},${result.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`
+                console.log(destiny)
+                getRouteTime(userpoint, destiny)
+                var routingParameters = {
+                    // The routing mode:
+                    'mode': `fastest;${$('#transport').val()};traffic:enabled`,
+                    // The start point of the route:
+                    'waypoint0': userpoint,
+                    // The end point of the route:
+                    'waypoint1': destiny,
+                    // To retrieve the shape of the route we choose the route
+                    // representation mode 'display'
+                    'representation': 'display',
+                };
 
-    };
+                // Define a callback function to process the routing response:
+                var onResult = function (result) {
+                    var route,
+                        routeShape,
+                        startPoint,
+                        endPoint,
+                        linestring;
 
-    // Define a callback function to process the routing response:
-    var onResult = function (result) {
-        var route,
-            routeShape,
-            startPoint,
-            endPoint,
-            linestring;
+                    if (result.response.route) {
+                        // Pick the first route from the response:
+                        route = result.response.route[0];
+                        // Pick the route's shape:
+                        routeShape = route.shape;
 
-        if (result.response.route) {
-            // Pick the first route from the response:
-            route = result.response.route[0];
-            console.log(route.leg[0].maneuver.reduce((acc, elem) => {
-                return acc + Number(elem.travelTime)
-            }, 0))
-            // Pick the route's shape:
-            routeShape = route.shape;
+                        // Create a linestring to use as a point source for the route line
 
-            // Create a linestring to use as a point source for the route line
-            linestring = new H.geo.LineString();
+                        linestring = new H.geo.LineString();
 
-            // Push all the points in the shape into the linestring:
-            routeShape.forEach(function (point) {
-                var parts = point.split(',');
-                linestring.pushLatLngAlt(parts[0], parts[1]);
-            });
+                        // Push all the points in the shape into the linestring:
+                        routeShape.forEach(function (point) {
+                            var parts = point.split(',');
+                            linestring.pushLatLngAlt(parts[0], parts[1]);
+                        });
 
-            // Retrieve the mapped positions of the requested waypoints:
-            startPoint = route.waypoint[0].mappedPosition;
-            endPoint = route.waypoint[1].mappedPosition;
+                        // Retrieve the mapped positions of the requested waypoints:
+                        startPoint = route.waypoint[0].mappedPosition;
+                        endPoint = route.waypoint[1].mappedPosition;
 
-            // Create a polyline to display the route:
-            var routeLine = new H.map.Polyline(linestring, {
-                style: { strokeColor: 'blue', lineWidth: 10 }
-            });
+                        // Create a polyline to display the route:
+                        var routeLine = new H.map.Polyline(linestring, {
+                            style: { strokeColor: 'blue', lineWidth: 10 }
+                        });
+                        var endMarker = new H.map.Marker({
+                            lat: endPoint.latitude,
+                            lng: endPoint.longitude
+                        });
 
-            // Create a marker for the start point:
-            var startMarker = new H.map.Marker({
-                lat: startPoint.latitude,
-                lng: startPoint.longitude
-            });
+                        // Add the route polyline and the two markers to the map:
+                        map.addObjects([routeLine, endMarker]);
 
-            // Create a marker for the end point:
-            var endMarker = new H.map.Marker({
-                lat: endPoint.latitude,
-                lng: endPoint.longitude
-            });
+                        // Set the map's viewport to make the whole route visible:
+                        map.setViewBounds(routeLine.getBounds());
+                    }
+                };
+                var router = platform.getRoutingService();
+                // Call calculateRoute() with the routing parameters,
+                // the callback and an error callback function (called if a
+                // communication error occurs):
+                router.calculateRoute(routingParameters, onResult,
+                    function (error) {
+                        alert(error.message);
+                    });
+                // })
+            })
+    })
 
-            // Add the route polyline and the two markers to the map:
-            map.addObjects([routeLine, startMarker, endMarker]);
-
-            // Set the map's viewport to make the whole route visible:
-            map.setViewBounds(routeLine.getBounds());
-        }
-    };
-
-    // Get an instance of the routing service:
-    var router = platform.getRoutingService();
-    // Call calculateRoute() with the routing parameters,
-    // the callback and an error callback function (called if a
-    // communication error occurs):
-    router.calculateRoute(routingParameters, onResult,
-        function (error) {
-            alert(error.message);
-        });
 }
 
-getSearchParams()
+function getRoute(userPosition) {
+
+
+}
+
+function searchDestiny() {
+    $("#address-search").on('click', (e) => {
+        e.preventDefault();
+    })
+}
+getLocation()
--- public/src/podcasts.js
+++ public/src/podcasts.js
@@ -1,6 +1,67 @@
-function listOfEpisodes(idPodcast) {
+// https://stackoverflow.com/questions/45483759/cannot-load-deezer-api-resources-from-localhost-with-the-fetch-api
+// const apiDeezer = `https://cors-anywhere.herokuapp.com/https://api.deezer.com`
 
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/podcast/${idPodcast}/episodes`)
-        .then(res => res.json()).then(res => console.log(res.data[0]));
+
+// function listOfEpisodes(idPodcast) {
+//     fetch(`${apiDeezer}`)
+//         .then(res => res.json()).then(res => console.log(res.data));
+// }
+// const myPodcasts = [1833, 2939, 2785, 3161, 2045, 1773, 91, 4319, 65, 1653, 4363, 9153, 27, 8381, 9955]
+const myPodcasts = [1833, 2939, 2785, 3161, 2045, 1773, 91, 4319, 65, 1653, 4363, 9153, 27, 8381, 9955]
+
+function findMatchingTime() {
+  const b = myPodcasts.map(async (podcastId) => {
+    const a = await getData(podcastId)
+    return a;
+  })
+  return (b)
 }
-listOfEpisodes('1833')
+
+async function getData(podcastId) {
+  const a = await fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/podcast/${podcastId}/episodes`)
+  const data = await a.json()
+  return data;
+}
+
+findMatchingTime().forEach(
+  prom => {
+    prom.then(response =>
+      response.data.forEach(elem => console.log(elem))
+    )
+  })
+console.log(findMatchingTime())
+// listOfEpisodes('1833')
+// //
+
+// DZ.init({
+//     appId: '349024',
+//     channelUrl: 'http://localhost:8080/channel.html'
+// });
+
+// DZ.api('/podcast/1833/episodes', response => {
+//     console.log(response);
+// });
+
+
+// myPodcasts.forEach(async (podcastId) => {
+//     const data = await DZ.api(`/podcast/${podcastId}/episodes`,
+//         response => {
+//             a.push(response.data)
+
+//         });
+//     console.log("data", data)
+// });
+// console.log("newarray", a)
+
+// function getData() {
+//     let a = [];
+//     myPodcasts.forEach((podcastId) => {
+//         DZ.api(`/podcast/${podcastId}/episodes`,
+//             response => response.data)
+
+//     });
+//     return a
+// }
+// getData()
+
+// const flatten = (a, b) => [...a, ...b];
