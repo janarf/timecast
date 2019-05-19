1e923aff9dbbac331ded3f5907d036c11f8531ea
janarf <janarf@hotmail.com>
2019-05-18 21:44:39 -0300

    três botoes seletores de caminho funcionando

--- public/src/maps.js
+++ public/src/maps.js
@@ -1,5 +1,6 @@
+const transport = ['car', 'bicycle', 'pedestrian'];
+
 function getRouteTime(waypointString0, waypointString1) {
-  const transport = ['car', 'bicycle', 'pedestrian'];
   transport.forEach(
     mean => {
       fetch(`https://route.api.here.com/routing/7.2/calculateroute.json
@@ -13,17 +14,17 @@ function getRouteTime(waypointString0, waypointString1) {
         .then(res => {
           if (mean == 'car') {
             $(`#${mean}-time`).html(
-              moment.utc(res.response.route[0].summary.trafficTime * 1000).format('HH:mm'))
+              moment.utc(res.response.route[0].summary.trafficTime * 1000)
+                .format('HH:mm'))
           } else {
             $(`#${mean}-time`).html(
-              moment.utc(res.response.route[0].summary.baseTime * 1000).format('HH:mm'))
+              moment.utc(res.response.route[0].summary.baseTime * 1000)
+                .format('HH:mm'))
           }
         })
     }
   )
 }
-
-const transportMean = 'car'
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -32,8 +33,13 @@ function getLocation() {
     console.log("Geolocation is not supported by this browser.")
   }
 }
-
 function getPosition(userPosition) {
+  localStorage.setItem("latitude", userPosition.coords.latitude)
+  localStorage.setItem("longitude", userPosition.coords.longitude)
+}
+
+function createMap() {
+  $('#mapContainer').html();
   var platform = new H.service.Platform({
     'app_id': 'lT3yqAJmOo0tSCEDAY24',
     'app_code': 'fd9_LnoGgmj6hkcLl2RhNQ'
@@ -43,104 +49,77 @@ function getPosition(userPosition) {
     $('#mapContainer')[0],
     defaultLayers.normal.map, {
       zoom: 13,
-      center: { lat: userPosition.coords.latitude, lng: userPosition.coords.longitude }
+      center: { lat: localStorage.getItem("latitude"), lng: localStorage.getItem("longitude") }
     });
   var marker = new H.map.Marker({
-    lat: userPosition.coords.latitude,
-    lng: userPosition.coords.longitude
+    lat: localStorage.getItem("latitude"),
+    lng: localStorage.getItem("longitude")
   });
   map.addObjects([marker])
   var ui = H.ui.UI.createDefault(map, defaultLayers);
+}
 
+function getRoute(transportMean) {
 
-  $("#address-search").on('click', (e) => {
-    e.preventDefault();
-    var geocodingParams = {
-      searchText: $("#address-search-bar").val().replace(/(-)|(,)/g, '')
-    };
-    var geocoder = platform.getGeocodingService();
-    let userpoint = `geo!${userPosition.coords.latitude},${userPosition.coords.longitude}`
-    console.log($("#address-search-bar").val().replace(/(-)|(,)/g, '').split(' ').join('+'))
+  let userPoint = `geo!${localStorage.getItem("latitude")},${localStorage.getItem("longitude")}`
 
-    fetch(`https://geocoder.api.here.com/6.2/geocode.json?app_id=lT3yqAJmOo0tSCEDAY24&app_code=fd9_LnoGgmj6hkcLl2RhNQ&searchtext=${$("#address-search-bar").val().replace(/(-)|(,)/g, '').split(' ').join('+')}`)
-      .then(res => res.json()).then(result => {
-        let destiny = `geo!${result.Response.View[0].Result[0].Location.DisplayPosition.Latitude},${result.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`
+  fetch(`https://geocoder.api.here.com/6.2/geocode.json?app_id=lT3yqAJmOo0tSCEDAY24&app_code=fd9_LnoGgmj6hkcLl2RhNQ&searchtext=${$("#address-search-bar").val().replace(/(-)|(,)/g, '').split(' ').join('+')}`)
+    .then(res => res.json()).then(result => {
+      let destiny = `geo!${result.Response.View[0].Result[0].Location.DisplayPosition.Latitude},${result.Response.View[0].Result[0].Location.DisplayPosition.Longitude}`
 
-        getRouteTime(userpoint, destiny)
-        var routingParameters = {
-          // The routing mode:
-          'mode': `fastest;${transportMean};traffic:enabled`,
-          // The start point of the route:
-          'waypoint0': userpoint,
-          // The end point of the route:
-          'waypoint1': destiny,
-          // To retrieve the shape of the route we choose the route
-          // representation mode 'display'
-          'representation': 'display',
-        };
+      getRouteTime(userPoint, destiny);
 
-        // Define a callback function to process the routing response:
-        var onResult = function (result) {
-          var route,
-            routeShape,
-            startPoint,
-            endPoint,
-            linestring;
+      var routingParameters = {
+        'mode': `fastest;${transportMean};traffic:enabled`,
+        'waypoint0': userPoint,
+        'waypoint1': destiny,
+        'representation': 'display',
+      };
 
-          if (result.response.route) {
-            // Pick the first route from the response:
-            route = result.response.route[0];
-            // Pick the route's shape:
-            routeShape = route.shape;
+      var onResult = function (result) {
+        var route,
+          routeShape,
+          startPoint,
+          endPoint,
+          linestring;
 
-            // Create a linestring to use as a point source for the route line
+        if (result.response.route) {
+          route = result.response.route[0];
+          routeShape = route.shape;
+          linestring = new H.geo.LineString();
 
-            linestring = new H.geo.LineString();
-
-            // Push all the points in the shape into the linestring:
-            routeShape.forEach(function (point) {
-              var parts = point.split(',');
-              linestring.pushLatLngAlt(parts[0], parts[1]);
-            });
-
-            // Retrieve the mapped positions of the requested waypoints:
-            startPoint = route.waypoint[0].mappedPosition;
-            endPoint = route.waypoint[1].mappedPosition;
-
-            // Create a polyline to display the route:
-            var routeLine = new H.map.Polyline(linestring, {
-              style: { strokeColor: 'blue', lineWidth: 10 }
-            });
-            var endMarker = new H.map.Marker({
-              lat: endPoint.latitude,
-              lng: endPoint.longitude
-            });
-
-            // Add the route polyline and the two markers to the map:
-            map.addObjects([routeLine, endMarker]);
-
-            // Set the map's viewport to make the whole route visible:
-            map.setViewBounds(routeLine.getBounds());
-          }
-        };
-        var router = platform.getRoutingService();
-        // Call calculateRoute() with the routing parameters,
-        // the callback and an error callback function (called if a
-        // communication error occurs):
-        router.calculateRoute(routingParameters, onResult,
-          function (error) {
-            alert(error.message);
+          routeShape.forEach(function (point) {
+            var parts = point.split(',');
+            linestring.pushLatLngAlt(parts[0], parts[1]);
           });
-        // })
-      })
-  })
+
+          startPoint = route.waypoint[0].mappedPosition;
+          endPoint = route.waypoint[1].mappedPosition;
+          var routeLine = new H.map.Polyline(linestring, {
+            style: { strokeColor: 'blue', lineWidth: 10 }
+          });
+          var endMarker = new H.map.Marker({
+            lat: endPoint.latitude,
+            lng: endPoint.longitude
+          });
+          var startMarker = new H.map.Marker({
+            lat: localStorage.getItem("latitude"),
+            lng: localStorage.getItem("longitude")
+          });
+          map.addObjects([startMarker, routeLine, endMarker]);
+          map.setViewBounds(routeLine.getBounds());
+
+        }
+      };
+      var router = platform.getRoutingService();
+      router.calculateRoute(routingParameters, onResult,
+        function (error) {
+          alert(error.message);
+        });
+    })
 
 }
 
-function getRoute(userPosition) {
-
-
-}
 
 function searchDestiny() {
   $("#address-search").on('click', (e) => {
@@ -148,3 +127,47 @@ function searchDestiny() {
   })
 }
 getLocation()
+
+var platform = new H.service.Platform({
+  'app_id': 'lT3yqAJmOo0tSCEDAY24',
+  'app_code': 'fd9_LnoGgmj6hkcLl2RhNQ'
+});
+var defaultLayers = platform.createDefaultLayers();
+let map = new H.Map(
+  $('#mapContainer')[0],
+  defaultLayers.normal.map, {
+    zoom: 13,
+    center: { lat: localStorage.getItem("latitude"), lng: localStorage.getItem("longitude") }
+  });
+var marker = new H.map.Marker({
+  lat: localStorage.getItem("latitude"),
+  lng: localStorage.getItem("longitude")
+});
+map.addObjects([marker])
+
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+$("#address-search").on('click', (e) => {
+  e.preventDefault();
+  getRoute('car');
+  $(`#car`).addClass('transport-button--clicked').removeClass('transport-button')
+
+})
+
+function addClickEventTransport(mean) {
+  $(`#${mean}`).on('click', () => {
+    transport.filter((element) => element !== mean)
+      .forEach(otherMean => $(`#${otherMean}`)
+        .addClass('transport-button')
+        .removeClass('transport-button--clicked'));
+
+    $(`#${mean}`).addClass('transport-button--clicked')
+      .removeClass('transport-button');
+
+    map.removeObjects(map.getObjects());
+    getRoute(mean);
+  })
+}
+
+
+transport.forEach(mean => addClickEventTransport(mean))
