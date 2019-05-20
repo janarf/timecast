30a5f373af57ab0389244e462723cfaec876e542
tmcamillo <imeio.tmc@gmail.com>
2019-05-20 19:30:35 -0300

    nav e tab feitos no bootstrap

---
+++ public/tabs.js
@@ -0,0 +1,19 @@
+const wayTab = document.querySelector("#way-tab");
+const timerTab = document.querySelector("#timer-tab");
+const firstList = document.querySelector("#f-list");
+const sectList = document.querySelector("#s-list");
+
+const displayTimerContent = document.querySelector("#timer-tab");
+displayTimerContent.addEventListener("click", () => {
+    wayTab.classList.remove("clicked-tab");
+    timerTab.classList.add("clicked-tab");
+
+});
+
+const displayWayContent = document.querySelector("#way-tab");
+displayWayContent.addEventListener("click", () => {
+
+    timerTab.classList.remove("clicked-tab");
+    wayTab.classList.add("clicked-tab");
+
+});
