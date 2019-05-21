51da1af59fb4ad368ff00103db3a97c6fa46077d
jupottes <juliettepottes@gmail.com>
2019-05-21 13:30:35 -0300

    layout page

---
+++ categories_script.js
@@ -0,0 +1,5 @@
+const allCateg = document.querySelectorAll("input[type=checkbox]");
+console.log(allCateg);
+const checkCateg = document.querySelectorAll("input[type=checkbox]:checked");
+const selectCategories = document.getElementById('btntest').onclick;
+
